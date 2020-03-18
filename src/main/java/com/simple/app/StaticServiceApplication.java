40bdbdb3f36e0278f41ecc33720451ad41970e0c
kuaile-david <853538344@qq.com>
2020-03-18 12:34:31 +0800

    123

--- src/main/java/com/simple/app/StaticServiceApplication.java
+++ src/main/java/com/simple/app/StaticServiceApplication.java
@@ -10,7 +10,6 @@ public class StaticServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StaticServiceApplication.class, args);
-		System.out.println();
 	}
 
 }
