5a286cfc4d3a767e3b6173fa749cc2c99d4a34b7
kuaile-david <59818906+kuaile-david@users.noreply.github.com>
2020-03-25 21:01:49 +0800

    ceshi

--- src/main/java/com/simple/app/StaticServiceApplication.java
+++ src/main/java/com/simple/app/StaticServiceApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StaticServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StaticServiceApplication.class, args);
+		SpringApplication.run(StaticServiceApplication.class, args);     
+		
 	}
 
 }
